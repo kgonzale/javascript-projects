f21b002375106fd3464bfd61913a476fe8254dac
Kevin Gonzalez <kgonzale@cbu.edu>
2018-10-15 23:48:07 -0500

    made cleaner

--- calculator_tip/calculator.js
+++ calculator_tip/calculator.js
@@ -1,13 +1,13 @@
-const tipCalculation = () => {
-  const total = document.getElementById("userInput").value;
+const totalTip = () => {
+  const input = document.getElementById("userInput").value;
   const service = document.getElementById("serviceQuality").value;
 
-  if (total > 0 && service > 0) {
-    const totalTip = total * service;
-    document.getElementById("showOutput").innerHTML = totalTip;
+  if (input > 0 && service > 0) {
+    const tipTotal = input * service;
+    document.getElementById("calculation").innerHTML = tipTotal;
   }
 };
 
 document.getElementById("calculate").onclick = () => {
-  tipCalculation();
+  totalTip();
 };
